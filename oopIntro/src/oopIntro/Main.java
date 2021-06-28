8d9fc73f603de66bbd5c9864272941a5040719f9
Vehbi Erbil Karakuş <80065498+demirkarakus@users.noreply.github.com>
2021-06-28 15:53:05 +0300

    oop Lessons

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -3,58 +3,58 @@ package oopIntro;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Product product1 = new Product();
-		product1.id = 1;
-		product1.name = "Lenova";
-		product1.unitPrice = 1500;
-		product1.details = "16 GB Ram";
-		
-		Product product2 = new Product();
-		product2.id = 2;
-		product2.name = "Casper";
-		product2.unitPrice = 2500;
-		product2.details = "64 GB Ram";
-		
-		Product product3 = new Product();
-		product3.id = 3;
-		product3.name = "Toshiba";
-		product3.unitPrice = 4500;
-		product3.details = "128 GB Ram";
-		
-		Product[] products = {product1, product2, product3};
-		
-		for(Product product : products) {
-			System.out.println(product.name);
-		}
-		
-		System.out.println(products.length);
 
-		Category category = new Category(1,"Araba");
-		System.out.println(category.name);
-		
+		Product product1 = new Product(1, "Lenova", 1500, "16 GB Ram", 10);
+
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Toshiba");
+		product2.setUnitPrice(12800);
+		product2.setDetails("16GB Ram");
+		product2.setDiscount(10);
+
+		Product product3 = new Product();
+		product3.setId(2);
+		product3.setName("Casper");
+		product3.setUnitPrice(14500);
+		product3.setDetails("32GB Ram");
+		product3.setDiscount(10);
+
+		Product[] products = { product1, product2, product3 };
+
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+
 		System.out.println("************************************************");
-		
+
+		Category category1 = new Category();
+		category1.setId(1);
+		category1.setName("Araba");
+		System.out.println(category1.getName());
+
+		System.out.println("************************************************");
+
 		ProductManager productMan = new ProductManager();
 		productMan.addToCart(product1);
 		productMan.addToCart(product2);
 		productMan.addToCart(product3);
-		
+
 		System.out.println("Ödev 2 ************************************************");
 
 		Courses courses1 = new Courses(1, "Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR)", "Engin Demiroğ");
 		Courses courses2 = new Courses(2, "Yazılım Geliştirici Yetiştirme Kampı (JAVA + REACT)", "Engin Demiroğ");
 		Courses courses3 = new Courses(3, "Programlamaya Giriş için Temel Kurs", "Engin Demiroğ");
-		
+
 		CourseManager courseMan = new CourseManager();
 		courseMan.addToLesson(courses1);
 		courseMan.addToLesson(courses2);
 		courseMan.addToLesson(courses3);
-		
+
 		System.out.println("************************************************");
 
-		Courses[] courses = {courses1, courses2, courses3};
-		for(Courses course : courses) {
+		Courses[] courses = { courses1, courses2, courses3 };
+		for (Courses course : courses) {
 			System.out.println(course.courseId + " -> " + course.courseName + " -> " + course.teacherName);
 		}
 	}
--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -2,9 +2,69 @@ package oopIntro;
 
 public class Product {
 	
-	int id;
-	String name;
-	double unitPrice;
-	String details;
+	//encapsulation
+	private int id;
+	private String name;
+	private double unitPrice;
+	private String details;
+	private double discount;
+	
+	
+	public Product() {
+		
+	}
+
+	public Product(int id, String name, double unitPrice, String details, double discount) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.details = details;
+		this.discount = discount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public double getUnitPriceAfterDiscount() {
+		return this.unitPrice - (unitPrice * this.discount / 100);
+	}
 	
 }
