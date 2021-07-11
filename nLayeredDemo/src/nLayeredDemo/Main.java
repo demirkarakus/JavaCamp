91afd48ad648627cf13b19e31dd17f4ae8b5a600
Vehbi Erbil Karakuş <80065498+demirkarakus@users.noreply.github.com>
2021-07-12 00:09:14 +0300

    Day 5 lesson added

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -2,7 +2,8 @@ package nLayeredDemo;
 
 import nLayeredDemo.business.abstracts.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
-import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.core.jLoggerManagerAdapter;
+import nLayeredDemo.dataAccess.concretes.AbcProductDao;
 import nLayeredDemo.entities.concretes.Product;
 
 public class Main {
@@ -10,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//ToDo : Spring IoC
-		ProductService productService = new ProductManager(new HibernateProductDao());
+		ProductService productService = new ProductManager(new AbcProductDao(), new jLoggerManagerAdapter());
 
 		Product product = new Product(1, 2, "Apple", 12, 50);
 		productService.add(product);
